77954b5fed11d25517b1f6497e9ad45ac5fbd23e
cedric <cicatriz.r00t@gmail.com>
2018-05-04 16:22:37 -0600

    no history data

--- build.sh
+++ build.sh
@@ -214,6 +214,26 @@ if [ $? -eq 0 ];then
   mv hosts /etc/hosts
 fi
 
+# no history
+hists=(
+    ".bash_history" 
+    ".sh_history" 
+    ".zsh_history" 
+)
 
+history -c
 
+for f in ${hists[*]};do
+    if [ -f ~/$f ]; then
+        file ~/$f
+        gshred -n 1 -u ~/$f
+        ln -s /dev/null ~/$f
+    fi
+done
+
+if [ -e ~/.bash_sessions ];then
+    rm -rf ~/.bash_sessions
+fi
+
+# done
 exit 0
